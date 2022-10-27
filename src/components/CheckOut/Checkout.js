48350f4216d4ac4029db7cd7148e05f0972ccec9
Md Aslam Ali <mdaslamali675@gmail.com>
2022-10-27 17:42:21 +0600

    update login form

--- src/components/CheckOut/Checkout.js
+++ src/components/CheckOut/Checkout.js
@@ -7,8 +7,8 @@ const Checkout = () => {
     const { img, name } = checkout;
     return (
         <div className='checkout-page text-center'>
-            <h1 className='my-4'>Thank you for your course</h1>
-            <h3>You Have Selected For <span className='bg-danger px-3 py-2 rounded text-white'>{name}</span></h3>
+            <h1>Thank you for your course</h1>
+            <h3>You Have Selected For <span className='bg-danger rounded text-white'>{name}</span></h3>
             <img src={img} alt="" className='rounded mt-5' />
             <Link to="/courses" className='d-block'><button className='btn btn-danger mt-5'>Back to your courses</button></Link>
         </div>
