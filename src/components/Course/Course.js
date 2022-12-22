462b5274f9112fb88df2f906ab0ca0640edc4838
mdaslamalidu <thehometutor2022@gmail.com>
2022-12-22 21:38:20 +0600

    update theme

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -9,7 +9,7 @@ const Course = () => {
   const { img, title, desc, name, duration, price, _id } = singleCourse;
   return (
     <div className="bg-color">
-      <div className="text-end mt-3 me-3">
+      <div className="text-end pt-3 me-3">
         <Pdf targetRef={ref} filename="div-blue.pdf">
           {({ toPdf }) => (
             <button className="btn btn-danger" onClick={toPdf}>
@@ -23,7 +23,7 @@ const Course = () => {
           <h2 className="fs-4 text-danger">
             Welcome to Course details page!!!!!
           </h2>
-          <p className="">
+          <p className="course-para">
             If you are looking for several courses that you can opt for after
             your primary school studies this is the right place for you. You
             will find the right course that you can study from the list of
@@ -38,21 +38,22 @@ const Course = () => {
           alt=""
         />
       </div>
-      <div className="courseDetailsContainer mb-5" ref={ref}>
+
+      <div className="courseDetailsContainer" ref={ref}>
         <h1 className="text-danger mt-2 text-center mb-5">About This Course</h1>
         <div className="courseDetails">
           <img src={img} className="rounded" />
           <div className="course-items">
-            <h5>
+            <h5 className="course-para">
               Title: <small>{title}</small>
             </h5>
-            <h5>
+            <h5 className="course-para">
               Name: <small>{name}</small>
             </h5>
-            <h5>
+            <h5 className="course-para">
               Price: <small>{price} $</small>
             </h5>
-            <h5>
+            <h5 className="course-para">
               Duration: <small>{duration} Months</small>
             </h5>
             <Link to={`/checkout/${_id}`}>
@@ -62,7 +63,7 @@ const Course = () => {
             </Link>
           </div>
         </div>
-        <p className="mt-4 fs-5">{desc}</p>
+        <p className="mt-4 fs-5 course-para">{desc}</p>
       </div>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,69 +1,89 @@
-import React, { useContext, useState } from 'react';
-import { Image } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import { Image } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 import "./Header.css";
 import { FaMoon, FaSun, FaUser } from "react-icons/fa";
-import { toast } from 'react-toastify';
-import { AuthContext } from '../../Context/UseContext';
-
+import { toast } from "react-toastify";
+import { AuthContext } from "../../Context/UseContext";
+import { useEffect } from "react";
 
 const Header = () => {
-    const { user, logout } = useContext(AuthContext)
-    const [theme, setTheme] = useState(false)
+  const { user, logout } = useContext(AuthContext);
+  const [myTheme, setMyTheme] = useState("dark-theme");
 
-    const handleLogout = () => {
-        logout().then(() => {
-            toast.success("successfully logout")
-        })
-        .catch(error => toast.error(error.message))
+  const handleTheme = () => {
+    if (myTheme === "dark-theme") {
+      setMyTheme("light-theme");
+    } else {
+      setMyTheme("dark-theme");
     }
+    console.log(myTheme);
+  };
 
-    return (
-        <Navbar expand="lg" className='sticky-top header-style'>
-            <Container>
-                <Link to="/" className='header-title'>
-                    <img style={{ height: "50px", borderRadius: "50%" }} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOrt6dYZqH63buWQAacIJDXWO4KAt6-exrwg&usqp=CAU" alt="" />
-                   It Survices Institute
-                </Link>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
-                    <Nav className='link-style align-items-center'>
-                        <Link to="/home">Home</Link>
-                        <Link to="/courses">Courses</Link>
-                        <Link to="/faq">Faq</Link>
-                        <Link to="/blog">Blog</Link>
-                        {
-                            user?.uid ? 
-                            <Link onClick={handleLogout}>Logout</Link>
-                            :
-                            <>
-                                    <Link to="/login">Login</Link>
-                                    <Link to="/register">Register</Link>
-                            </>
-                        }
-                        <Link to="/profile">
+  const handleLogout = () => {
+    logout()
+      .then(() => {
+        toast.success("successfully logout");
+      })
+      .catch((error) => toast.error(error.message));
+  };
 
-                            {
-                                user?.photoURL ?
-                                    <Image title={user?.displayName} roundedCircle src={user.photoURL} style={{ height: "40px", width: "40px", cursor: "pointer" }}></Image>
-                                    :
-                                    <FaUser></FaUser>
-                            }
-                        </Link>
+  useEffect(() => {
+    document.body.className = myTheme;
+  }, [myTheme]);
 
-                        <button className='theme-btn' onClick={() => setTheme(!theme)}>
-                            {theme? <FaMoon></FaMoon> : <FaSun></FaSun>}
-                        </button>
+  return (
+    <Navbar expand="lg" className="sticky-top header-style">
+      <Container>
+        <Link to="/" className="header-title">
+          <img
+            style={{ height: "50px", borderRadius: "50%" }}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOrt6dYZqH63buWQAacIJDXWO4KAt6-exrwg&usqp=CAU"
+            alt=""
+          />
+          It Survices Institute
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="link-style align-items-center">
+            <Link to="/home">Home</Link>
+            <Link to="/courses">Courses</Link>
+            <Link to="/faq">Faq</Link>
+            <Link to="/blog">Blog</Link>
+            {user?.uid ? (
+              <Link onClick={handleLogout}>Logout</Link>
+            ) : (
+              <>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+              </>
+            )}
+            <Link to="/profile">
+              {user?.photoURL ? (
+                <Image
+                  title={user?.displayName}
+                  roundedCircle
+                  src={user.photoURL}
+                  style={{ height: "40px", width: "40px", cursor: "pointer" }}
+                ></Image>
+              ) : (
+                <FaUser></FaUser>
+              )}
+            </Link>
 
-                        {/* https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2_JMhOGTiIiNQt0WyWI5rhTA6Qlbi92BM0wbrkC7k&s */}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+            <button className="theme-btn" onClick={handleTheme}>
+              {myTheme === "dark-theme" ? <FaMoon></FaMoon> : <FaSun></FaSun>}
+            </button>
+
+            {/* https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2_JMhOGTiIiNQt0WyWI5rhTA6Qlbi92BM0wbrkC7k&s */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
