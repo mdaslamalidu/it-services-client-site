8d1d0d66ff36f1f0e793ac844f9c327ad0c44849
mdaslamalidu <thehometutor2022@gmail.com>
2022-12-22 21:57:19 +0600

    update design

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,24 +1,23 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../../Context/UseContext';
+import React, { useContext } from "react";
+import { AuthContext } from "../../Context/UseContext";
+import "./Profile.css";
 
 const Profile = () => {
-    const {user} = useContext(AuthContext);
-    return (
-        <div style={{height: "500px"}} className='d-flex justify-content-center align-items-center my-5'>
-            {
-                user?.uid ? 
-                <div>
-                    <h2 className='text-center'>{user?.displayName}</h2>
-                    <img className='w-100 rounded-circle' src={user?.photoURL} alt="" />
-                </div>
-                : 
-                <div>
-                    <h1>please login to see about your information!!</h1>
-                </div>
-
-            }
+  const { user } = useContext(AuthContext);
+  return (
+    <div className="d-flex justify-content-center align-items-center my-5 profile-page">
+      {user?.uid ? (
+        <div>
+          <h2 className="text-center">{user?.displayName}</h2>
+          <img className="w-100 rounded-circle" src={user?.photoURL} alt="" />
         </div>
-    );
+      ) : (
+        <div>
+          <h1>please login to see about your information!!</h1>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Profile;
