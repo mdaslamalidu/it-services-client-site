0fa6824fb9e9f42bc52519eb48ca3ee3d0371146
mdaslamalidu <thehometutor2022@gmail.com>
2022-12-22 20:49:40 +0600

    add dark and light color

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,29 +1,40 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import { FaFacebook, FaGithub, FaTwitter } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Container } from "react-bootstrap";
+import { FaFacebook, FaGithub, FaTwitter } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <div style={{ backgroundColor: "#ECE6E6"}}>
-            <Container className='d-lg-flex flex-wrap justify-content-between align-items-center py-4'>
-                <div>
-                    <Link to="/" className='header-title fs-4'>
-                        <img style={{ height: "50px", borderRadius: "50%" }} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOrt6dYZqH63buWQAacIJDXWO4KAt6-exrwg&usqp=CAU" alt="" />
-                        It Survices Institute
-                    </Link>
-                </div>
-                <div>
-                    <h5 className='fs-6'>All Rights Reserved by Md Aslam</h5>
-                </div>
-                <div>
-                    <a href="https://www.facebook.com/mdaslamalidu"><FaFacebook></FaFacebook></a>
-                    <a href="https://twitter.com/MdAslam19990"><FaTwitter className='mx-4'></FaTwitter></a>
-                    <a href="https://github.com/mdaslamalidu"><FaGithub></FaGithub></a>
-                </div>
-            </Container>
+  return (
+    <div className="footerStyle">
+      <Container className="d-lg-flex flex-wrap justify-content-between align-items-center py-4">
+        <div>
+          <Link to="/" className="header-title fs-4">
+            <img
+              style={{ height: "50px", borderRadius: "50%" }}
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOrt6dYZqH63buWQAacIJDXWO4KAt6-exrwg&usqp=CAU"
+              alt=""
+            />
+            It Survices Institute
+          </Link>
         </div>
-    );
+        <div>
+          <h5 className="fs-6 footer-title">All Rights Reserved by Md Aslam</h5>
+        </div>
+        <div>
+          <a href="https://www.facebook.com/mdaslamalidu">
+            <FaFacebook></FaFacebook>
+          </a>
+          <a href="https://twitter.com/MdAslam19990">
+            <FaTwitter className="mx-4"></FaTwitter>
+          </a>
+          <a href="https://github.com/mdaslamalidu">
+            <FaGithub></FaGithub>
+          </a>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React from "react";
 import "./Home.css";
-import banner from "../../Assests/banner.jpg"
-import { Link } from 'react-router-dom';
-
+import banner from "../../Assests/banner.jpg";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div className='home-container'>
-            <div className='left-side'>
-                <h1>Build Your Carrier<br/> <span className='text-danger'>With The Best One</span></h1>
-                <Link to="/courses"><button className='btn btn-danger px-5'>Our Courses</button></Link>
-            </div>
-            <div className='right-side'>
-                <img className='' src={banner} alt="" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="home-container">
+      <div className="left-side">
+        <h1 className="text-white">
+          Build Your Carrier
+          <br /> <span className="text-danger">With The Best One</span>
+        </h1>
+        <Link to="/courses">
+          <button className="btn btn-danger px-5">Our Courses</button>
+        </Link>
+      </div>
+      <div className="right-side">
+        <img className="" src={banner} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
